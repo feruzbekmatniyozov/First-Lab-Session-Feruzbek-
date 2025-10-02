9a58909dde41c5aeba785d648b1fabaf813481bc
Feruzbek Matniyazov <ferko07uzb@gmail.com>
2025-10-02 15:09:26 +0500

    Introduction program

---
+++ intro.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+major = input("What is your major? ")
+hobby = input("what is your hobby? ")
+
+print("Hello, my name is " + name + "." + "\nMy major is " + major + "." + "\nMy hobby is " + hobby)
